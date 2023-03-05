2830c796507ab32e2715f76c74fd1ffbae4cfb8c
Miguel Chavez <miguelchavez.dev@gmail.com>
2023-03-05 16:35:46 -0300

    add api resources for user model

---
+++ api/app/Http/Resources/UserResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public bool $flag;
+
+    public function __construct($resource, bool $flag)
+    {
+        $this->resource = $resource;
+        $this->flag = $flag;
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $weather = false;
+        if ($this->flag) {
+            $weather = $this->getWeather();
+        }
+
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'lat' => $this->latitude,
+            'lng' => $this->longitude,
+            'weather' => $this->when($this->flag, $weather),
+        ];
+    }
+}
