9b49b0754e286945d04ff7bc29490df5b0692eef
Miguel Chavez <miguelchavez.dev@gmail.com>
2023-03-05 16:35:21 -0300

    create user function to retrieve weather

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -5,6 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Http\Services\Weather\{
+    WeatherRequester, 
+    OpenWeatherRequester, 
+    WeatherApiRequester
+};
+use Illuminate\Support\Facades\Cache;
 
 class User extends Authenticatable
 {
@@ -39,4 +45,22 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getWeather()
+    {
+        // $weather = $this->clientWeather(new OpenWeatherRequester(), $this);
+        $weather = $this->clientWeather(new WeatherApiRequester(), $this);
+
+        return $weather;
+    }
+
+    protected function clientWeather(WeatherRequester $provider, User $user)
+    {
+        $weather = Cache::remember("user_weather_{$user->id}", 3600, function () use ($provider, $user) {
+            $provider->setPosition($user->latitude, $user->longitude);
+            return $provider->getWeather();
+        });
+
+        return $weather;
+    }
 }
